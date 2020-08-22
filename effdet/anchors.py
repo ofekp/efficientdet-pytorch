3bfddf02abe56d8dcf6ec4549d58e02ae2ef2f4f
Ofek <ofekp@users.noreply.github.com>
2020-08-22 17:55:17 +0300

    Update anchors.py

--- effdet/anchors.py
+++ effdet/anchors.py
@@ -30,7 +30,7 @@ import torch
 import torch.nn as nn
 from torchvision.ops.boxes import batched_nms, remove_small_boxes
 
-from effdet.object_detection import ArgMaxMatcher, FasterRcnnBoxCoder, BoxList, IouSimilarity, TargetAssigner
+from object_detection import ArgMaxMatcher, FasterRcnnBoxCoder, BoxList, IouSimilarity, TargetAssigner
 
 # The minimum score to consider a logit for identifying detections.
 MIN_CLASS_SCORE = -5.0
@@ -207,21 +207,32 @@ def generate_detections(
     scores = cls_outputs.sigmoid().squeeze(1).float()
     top_detection_idx = batched_nms(boxes, scores, classes, iou_threshold=0.5)
 
-    # keep only topk scoring predictions
+    # keep only top k scoring predictions
     top_detection_idx = top_detection_idx[:max_det_per_image]
     boxes = boxes[top_detection_idx]
     scores = scores[top_detection_idx, None]
     classes = classes[top_detection_idx, None]
 
     # xyxy to xywh & rescale to original image
-    boxes[:, 2] -= boxes[:, 0]
-    boxes[:, 3] -= boxes[:, 1]
-    boxes *= img_scale
+    # TODO(ofekp): we use xyxy in the data loader
+    # boxes[:, 2] -= boxes[:, 0]
+    # boxes[:, 3] -= boxes[:, 1]
+    # TODO(ofekp): convert back to xyxy (at this point we are with yxyx)
+    cols_order = torch.tensor([1,0,3,2], dtype=torch.long).to(boxes.device)
+    boxes_yxyx = torch.index_select(boxes, 1, cols_order)
+    assert img_scale == 1.0
+    boxes_yxyx *= img_scale
 
-    classes += 1  # back to class idx with background class = 0
+    assert boxes[0][0] == boxes_yxyx[0][1]
+    assert boxes[0][1] == boxes_yxyx[0][0]
+    assert boxes[0][2] == boxes_yxyx[0][3]
+    assert boxes[0][3] == boxes_yxyx[0][2]
+
+    # TODO(ofekp): WHY?!
+    # classes += 1  # back to class idx with background class = 0
 
     # stack em and pad out to MAX_DETECTIONS_PER_IMAGE if necessary
-    detections = torch.cat([boxes, scores, classes.float()], dim=1)
+    detections = torch.cat([boxes_yxyx, scores, classes.float()], dim=1)
     if len(top_detection_idx) < max_det_per_image:
         detections = torch.cat([
             detections,
@@ -344,7 +355,8 @@ class AnchorLabeler(object):
         cls_targets, _, box_targets, _, matches = self.target_assigner.assign(anchor_box_list, gt_box_list, gt_labels)
 
         # class labels start from 1 and the background class = -1
-        cls_targets -= 1
+        print("cls_targets [{}]".format(cls_targets))
+        # cls_targets -= 1
         cls_targets = cls_targets.long()
 
         # Unpack labels.
@@ -392,11 +404,26 @@ class AnchorLabeler(object):
         for i in range(batch_size):
             last_sample = i == batch_size - 1
             # cls_weights, box_weights are not used
-            cls_targets, _, box_targets, _, matches = self.target_assigner.assign(
-                anchor_box_list, BoxList(gt_boxes[i]), gt_classes[i])
+            cols_order = torch.LongTensor([1,0,3,2]).to(gt_boxes[i].device)
+            gt_boxes_i_yxyx = torch.index_select(gt_boxes[i], 1, cols_order)
+            # print(gt_boxes[i])                                     # this is good
+            # print(torch.index_select(gt_boxes[i], 1, cols_order))  # this is good
 
+            assert torch.min(gt_classes[i]) >= 0
+            assert torch.max(gt_classes[i]) < 11  # num_classes
+
+            # checking only the fist box, just to validate it is doing what we think it does
+            assert gt_boxes[i][0][0] == gt_boxes_i_yxyx[0][1]
+            assert gt_boxes[i][0][1] == gt_boxes_i_yxyx[0][0]
+            assert gt_boxes[i][0][2] == gt_boxes_i_yxyx[0][3]
+            assert gt_boxes[i][0][3] == gt_boxes_i_yxyx[0][2]
+            cls_targets, _, box_targets, _, matches = self.target_assigner.assign(
+                # anchor_box_list, BoxList(gt_boxes[i]), gt_classes[i])
+                anchor_box_list, BoxList(gt_boxes_i_yxyx), gt_classes[i])  # TODO(ofekp): efficientdet is expecting boxes in yxyx format
             # class labels start from 1 and the background class = -1
-            cls_targets -= 1
+            # cls_targets -= 1  # TODO(ofekp): commented this out
+            assert torch.min(cls_targets) >= 0
+            assert torch.max(cls_targets) < 11  # num_classes
             cls_targets = cls_targets.long()
 
             # Unpack labels.
@@ -418,4 +445,3 @@ class AnchorLabeler(object):
                 num_positives_out = torch.stack(num_positives_out)
 
         return cls_targets_out, box_targets_out, num_positives_out
-
